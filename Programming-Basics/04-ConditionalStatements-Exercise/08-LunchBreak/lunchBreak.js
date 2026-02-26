d152842740bec7640641f3316ae689e2c84ea289
Ivan Ivanov <ivan78608@gmail.com>
2026-02-26 22:03:20 +0200

    Chore:(Conditional Statements-Exercise) Completed

--- Programming-Basics/04-ConditionalStatements-Exercise/08-LunchBreak/lunchBreak.js
+++ Programming-Basics/04-ConditionalStatements-Exercise/08-LunchBreak/lunchBreak.js
@@ -1,23 +1,20 @@
-function LunchBreak (tvSeries, episodeDuration, breakDuration) {
-    // Calculate the time needed for lunch
-    let breakTime = breakDuration * 1 / 8
+function LunchBreak(seriesName, episodeDuration, breakDuration) {
+    let timeForLunch = breakDuration / 8;
+    let timeForRelax = breakDuration / 4;
+    let timeLeft = breakDuration - timeForLunch - timeForRelax;
 
-    // Calculate the time needed for watching the episode
-    let episodeTime = breakTime * 1 / 4
-
-    // Calculate the remaining time after lunch and watching the episode
-    let remainingTime = breakDuration - breakTime - episodeTime;
-
-    // Check if the remaining time is enough to watch the episode
-    if (remainingTime >= episodeDuration) {
-        // Print result to the console
-        console.log(`You have enough time to watch ${tvSeries} and left with ${remainingTime.toFixed(0)} minutes free time.`);
+    // Check if the time is enough for watching the episode
+    if (timeLeft >= episodeDuration) {
+        let timeLeftAfterWatching = Math.ceil(timeLeft - episodeDuration);
+        
+        // Print to the console
+        console.log(`You have enough time to watch ${seriesName} and left with ${timeLeftAfterWatching} minutes free time.`);
     }
     else {
-        let neededTime = episodeDuration - remainingTime;
+        let timeNeeded = Math.ceil(episodeDuration - timeLeft);
         
-        // Print result to the console
-        console.log(`You don't have enough time to watch ${tvSeries}, you need ${neededTime.toFixed(0)} more minutes.`);
+        // Print to the console
+        console.log(`You don't have enough time to watch ${seriesName}, you need ${timeNeeded} more minutes.`);
     }
 }
 

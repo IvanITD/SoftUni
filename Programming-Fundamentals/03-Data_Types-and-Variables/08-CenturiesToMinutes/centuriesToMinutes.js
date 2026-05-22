0c977942595ae2a89377eca2af20941f6877a974
IvanITD <ivanivanovofficial@icloud.com>
2026-05-23 02:11:36 +0300

    Update on repo

--- Programming-Fundamentals/03-Data_Types-and-Variables/08-CenturiesToMinutes/centuriesToMinutes.js
+++ Programming-Fundamentals/03-Data_Types-and-Variables/08-CenturiesToMinutes/centuriesToMinutes.js
@@ -1,3 +1,13 @@
-function CenturiesToMinutes() {
-    
+function CenturiesToMinutes(centuries) {
+    let years = centuries * 100;
+    let days = years * 365.2422;
+    days = Math.trunc(days);
+
+    let hours = days * 24;
+    let minutes = hours * 60;
+
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
 }
+
+CenturiesToMinutes(1);
+CenturiesToMinutes(5);
--- Programming-Fundamentals/03-Data_Types-and-Variables/09-SpecialNumbers/specialNumbers.js
+++ Programming-Fundamentals/03-Data_Types-and-Variables/09-SpecialNumbers/specialNumbers.js
@@ -1,3 +1,16 @@
-function SpecialNumbers() {
+function SpecialNumbers(number) {
+    let sum = 0;
     
+    for (let i = 1; i <= number; i++) {
+        sum += i;
+
+        if (i === 5 || i === 7 || i === 14) {
+            console.log(`${i} -> True`);
+        } else {
+            console.log(`${i} -> False`);
+        }
+    }
 }
+
+SpecialNumbers(15);
+SpecialNumbers(20);

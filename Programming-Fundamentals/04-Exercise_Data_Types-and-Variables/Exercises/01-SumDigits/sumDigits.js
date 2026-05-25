d770797f583575bf9ef6fcdc6a7d3ca8607ecd40
Ivan Ivanov <ivan78608@gmail.com>
2026-05-25 03:36:59 +0300

    Update on the tasks

--- Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/01-SumDigits/sumDigits.js
+++ Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/01-SumDigits/sumDigits.js
@@ -1,4 +1,13 @@
-function SumDigits() {
-    
+function SumDigits(number) {
+    let sum = 0;
+    let numberAsString = String(number);
+
+    for (let i = 0; i < numberAsString.length; i++) {
+        sum += Number(numberAsString[i]);
+    }
+    console.log(sum);
 }
 
+SumDigits(245678);
+SumDigits(97561);
+SumDigits(543);
--- Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/02-AsciiValuesOfReversedCharacters/asciiValuesOfReversedCharacters.js
+++ Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/02-AsciiValuesOfReversedCharacters/asciiValuesOfReversedCharacters.js
@@ -1,3 +1,8 @@
-function ASCIIValuesOfReversedCharacters() {
-
+function ASCIIValuesOfReversedCharacters(char1, char2, char3) {
+    console.log(`${char3}${char2}${char1}`);
+    console.log(`${char3.charCodeAt(0)} ${char2.charCodeAt(0)} ${char1.charCodeAt(0)}`);
 }
+
+ASCIIValuesOfReversedCharacters('a', 'b', 'c');
+ASCIIValuesOfReversedCharacters('%', '2', 'o');
+ASCIIValuesOfReversedCharacters('1', '5', 'p');

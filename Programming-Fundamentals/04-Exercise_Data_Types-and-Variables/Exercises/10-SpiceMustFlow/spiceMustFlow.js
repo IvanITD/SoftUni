18ffdd5e1b5eb46aad59e070b1d902117a90c067
Ivan Ivanov <ivan78608@gmail.com>
2026-05-26 15:07:38 +0300

    Chore:(Exercises) All tasks solved!
    Solved all tasks from the Exercises: Data Types and Variables in the Programming Fundamentals

--- Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/10-SpiceMustFlow/spiceMustFlow.js
+++ Programming-Fundamentals/04-Exercise_Data_Types-and-Variables/Exercises/10-SpiceMustFlow/spiceMustFlow.js
@@ -1,3 +1,23 @@
 function SpiceMustFlow(startingYield) {
-    
+    let dayCounter = 0;
+    let spicesCollected = 0;
+
+    while (startingYield >= 100) {
+        dayCounter++; // Adding a day to the counter
+        spicesCollected += startingYield; // Adding the yield of the current day to the total collected spices
+        startingYield -= 10; // Each next day the yield decreases by 10
+        spicesCollected -= 26; // Each next day the workers consume 26 spices
+    }
+
+    // Because the yield ended the workers consume 26 more spices.
+    if (spicesCollected >= 26) {
+        spicesCollected -= 26;
+    }
+
+    // Print the result to the console
+    console.log(dayCounter);
+    console.log(spicesCollected);
 }
+
+SpiceMustFlow(111);
+SpiceMustFlow(450);

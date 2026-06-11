9d19be9285d907db7c1c26ab379a5e757f715362
IvanITD <ivanivanovofficial@icloud.com>
2026-06-12 01:44:50 +0300

    Chore:(Radio Crystals)Solve the task
    Solved one of the hardest tasks

--- Programming-Fundamentals/10-Exercise_Functions/MoreExercises/03-PointsValidation/pointsValidation.js
+++ Programming-Fundamentals/10-Exercise_Functions/MoreExercises/03-PointsValidation/pointsValidation.js
@@ -1,6 +1,41 @@
-function pointsValidation() {
+function pointsValidation(arr) {
+    let x1 = Number(arr[0]);
+    let y1 = Number(arr[1]);
+    let x2 = Number(arr[2]);
+    let y2 = Number(arr[3]);
+    
+    function isValid(x1, y1, x2, y2) {
+        let distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+        
+        return Number.isInteger(distance);
+    }
 
+    let firstCheck = isValid(x1, y1, 0, 0);
+    let secondCheck = isValid(x2, y2, 0, 0);
+    let thirdCheck = isValid(x1, y1, x2, y2);
+
+    if (firstCheck === true) {
+        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is valid`);
+    }
+    else {
+        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is invalid`);
+    }
+
+    if (secondCheck === true) {
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is valid`);
+    }
+    else {
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is invalid`);
+    }
+
+    if (thirdCheck === true) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    }
+    else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
 }
 
 pointsValidation([3, 0, 0, 4]);
+console.log();
 pointsValidation([2, 1, 1, 1]);
--- Programming-Fundamentals/10-Exercise_Functions/MoreExercises/04-RadioCrystals/radioCrystals.js
+++ Programming-Fundamentals/10-Exercise_Functions/MoreExercises/04-RadioCrystals/radioCrystals.js
@@ -1,6 +1,71 @@
-function radioCrystals() {
+function radioCrystals(arr) {
+    let neededThickness = arr[0];  
 
+    function cut(x) { return x / 4; }
+    function lap(x) { return x - x * 0.2; }
+    function grind(x) { return x - 20; }
+    function etch(x) { return x - 2; }
+    function xray(x) { return x + 1; }
+    
+    for (let i = 1; i < arr.length; i++) {  
+        let currentThickness = arr[i];       
+        console.log(`Processing chunk ${currentThickness} microns`);
+
+        while (currentThickness !== neededThickness) {
+            let cutCount = 0;
+            let lapCount = 0;
+            let grindCount = 0;
+            let etchCount = 0;
+
+            while ((currentThickness / 4) >= neededThickness) {
+                currentThickness = cut(currentThickness);
+                cutCount++;
+            }
+            if (cutCount > 0) {
+                console.log(`Cut x${cutCount}`);
+                console.log(`Transporting and washing`);
+                currentThickness = Math.floor(currentThickness);
+            }
+
+            while ((currentThickness - currentThickness * 0.2) >= neededThickness) {
+                currentThickness = lap(currentThickness);
+                lapCount++;
+            }
+            if (lapCount > 0) {
+                console.log(`Lap x${lapCount}`);
+                console.log(`Transporting and washing`);
+                currentThickness = Math.floor(currentThickness);
+            }
+
+            while ((currentThickness - 20) >= neededThickness) {
+                currentThickness = grind(currentThickness);
+                grindCount++;
+            }
+            if (grindCount > 0) {
+                console.log(`Grind x${grindCount}`);
+                console.log(`Transporting and washing`);
+                currentThickness = Math.floor(currentThickness);
+            }
+
+            while ((currentThickness - 2) >= neededThickness - 1) {
+                currentThickness = etch(currentThickness);
+                etchCount++;
+            }
+            if (etchCount > 0) {
+                console.log(`Etch x${etchCount}`);
+                console.log(`Transporting and washing`);
+                currentThickness = Math.floor(currentThickness);
+            }
+
+            if (currentThickness < neededThickness) {
+                currentThickness = xray(currentThickness);
+                console.log(`X-ray x1`);
+            }
+        }
+        console.log(`Finished crystal ${neededThickness} microns`);
+    }
 }
 
 radioCrystals([1375, 50000]);
+console.log();
 radioCrystals([1000, 4000, 8100]);

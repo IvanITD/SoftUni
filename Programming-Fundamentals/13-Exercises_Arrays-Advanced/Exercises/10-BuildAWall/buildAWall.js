8d997dc4385c700d110c5d164ecaa20fa1360fdd
Ivan Ivanov <ivan78608@gmail.com>
2026-06-20 07:44:37 +0300

    Chore:(Exercises) Solve all tasks
    Solved all the tasks from 1 to 10 from the Arrays
    Advanced Exercises
    All the tasks are already given in the Judge system.
    Will further upload the solutions from the Arrays
    Advanced - More Exercises

--- Programming-Fundamentals/13-Exercises_Arrays-Advanced/Exercises/10-BuildAWall/buildAWall.js
+++ Programming-Fundamentals/13-Exercises_Arrays-Advanced/Exercises/10-BuildAWall/buildAWall.js
@@ -1,3 +1,28 @@
-function buildAWall() {
-    
+function buildAWall(arr) {
+    let sections = arr.map(Number);
+
+    let dailyConcrete = [];
+
+    while (sections.some(section => section < 30)) {
+        let crewsWorking = 0;
+        
+        for (let i = 0; i < sections.length; i++) {
+            if (sections[i] < 30) {
+                sections[i]++;
+                crewsWorking++;
+            }
+        }
+
+        dailyConcrete.push(crewsWorking * 195);
+    }
+
+    console.log(dailyConcrete.join(', '));
+    let totalConcrete = dailyConcrete.reduce((a, b) => a + b, 0);
+    console.log(totalConcrete * 1900 + ' pesos');
 }
+
+buildAWall([21, 25, 28]);
+console.log();
+buildAWall([17]);
+console.log();
+buildAWall([17, 22, 17, 19, 17]);
